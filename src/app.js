13796cb8e9f0b214978bbd14f24e8f5fe9807468
vedant0000 <hulevedant@gmail.com>
2025-12-31 03:39:55 +0530

    Add user registration and login with JWT authentication

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
+const authRoutes = require("./routes/auth.routes");
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
 
 app.get("/",  (req, res) => {
     res.send("API is running");
